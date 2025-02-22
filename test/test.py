7c8ba2697c33e2a35898eb08648de4d2f185e035
RVCE-ECE-Shylashree <drshylashreen@gmail.com>
2025-02-22 23:16:06 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,14 +4,14 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
-
+from cocotb.triggers import Timer 
 
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    # Set the clock period to 10 ns (100 MHz)
+    clock = Clock(dut.clk, 20, units="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -20,21 +20,30 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 1)
+    #await Timer(5, units="ns")
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.ui_in.value = 0x00
+    dut.uio_in.value = 0x00
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
+    #await Timer(20, units="ns")
+    dut.ui_in.value = 0x3E
+    dut.uio_in.value = 0x42
 
+    await ClockCycles(dut.clk, 11)
+    #await Timer(205, units="ns")
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.uo_out.value == 0xDA
 
+    #await ClockCycles(dut.clk, 1)
+    await Timer(20, units="ns")
+    assert dut.uo_out.value == 0x44
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
